e304b7b94aca882d10355f6393781186fe04adb5
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-26 13:44:17 +0200

    Add restart game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let board = initialGameBoard;
+  let board = [...initialGameBoard.map((innerArray) => [...innerArray])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -61,6 +61,9 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
   return (
     <main>
       <div id="game-container">
@@ -76,7 +79,9 @@ function App() {
             isActive={activePlayer === '0'}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard onSelect={handleSelectSquare} board={board} />
       </div>
       <Log turns={gameTurns} />
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,11 +1,11 @@
-export default function GameOver({ winner }) {
+export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over</h2>
       {winner && <p>You won, {winner}</p>}
       {!winner && <p>It's draw</p>}
       <p>
-        <button>Restart</button>
+        <button onClick={onRestart}>Restart</button>
       </p>
     </div>
   );
