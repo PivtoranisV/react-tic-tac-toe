db8a07ee0029a8de9cb65d56b604e0d7ca256dbb
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-26 12:09:54 +0200

    Update Active player state managing

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = '0';
+  }
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer(() => (activePlayer === 'X' ? '0' : 'X'));
+  const activePlayer = deriveActivePlayer(gameTurns);
 
+  function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-      if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-        currentPlayer = '0';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
