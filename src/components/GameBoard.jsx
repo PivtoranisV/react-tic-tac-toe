88ed53244ddfcad2e8aa23a68341254e90971617
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-26 10:23:47 +0200

    Add functionality to update board on click

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,26 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [board, setBoard] = useState(initialGameBoard);
+
+  function handleClick(rowIndex, colIndex) {
+    setBoard((prevBoard) => {
+      const updatedBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
